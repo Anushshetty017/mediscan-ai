1facca162c1410c07900e405e4cb932350703198
Anushshetty017 <TeraEmail@domain.com>
2026-04-19 23:18:50 +0530

    fix run config

--- app.py
+++ app.py
@@ -179,4 +179,4 @@ def predict_route():
 # ENTRY POINT
 # ==========================================
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
